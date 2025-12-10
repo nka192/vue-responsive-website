7b6a8f22a35bde2d72de5bc1394d71aa8c8f887f
Nayoung Kim <nka50@sfu.ca>
2025-12-09 16:59:23 -0800

    feat(layout): add DefaultLayout.vue with header, footer, and router-view

---
+++ vue-responsive-website/src/components/layout/DefaultLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="min-h-screen flex flex-col relative">
+    <!-- Header -->
+    <PageHeader />
+
+    <!-- Main Content -->
+    <main class="flex-grow">
+      <router-view></router-view>
+    </main>
+
+    <!-- Footer -->
+    <PageFooter />
+  </div>
+</template>
+
+<script>
+import PageHeader from "./PageHeader.vue";
+import PageFooter from "./PageFooter.vue";
+
+export default {
+  components: {
+    PageHeader,
+    PageFooter,
+  },
+};
+</script>
+
+<style></style>
