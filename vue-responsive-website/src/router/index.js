82eb86d8639a222553207c06881cc75c6d84cdfb
Nayoung Kim <nka50@sfu.ca>
2025-12-10 18:53:11 -0800

    feat(router): add routes for all pages

--- vue-responsive-website/src/router/index.js
+++ vue-responsive-website/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DefaultLayout from "../components/layout/DefaultLayout.vue";
 import HomePage from "../page/HomePage.vue";
+import AboutPage from "../page/AboutPage.vue";
+import ServicesPage from "../page/ServicesPage.vue";
+import PricingPage from "../page/PricingPage.vue";
+import CarsPage from "../page/CarsPage.vue";
+import BlogPage from "../page/BlogPage.vue";
+import ContactPage from "../page/ContactPage.vue";
 
 const routes = [
   {
@@ -11,6 +17,36 @@ const routes = [
         path: "",
         name: "home",
         component: HomePage,
+      },
+      {
+        path: "/about",
+        name: "about",
+        component: AboutPage,
+      },
+      {
+        path: "/services",
+        name: "services",
+        component: ServicesPage,
+      },
+      {
+        path: "/pricing",
+        name: "pricing",
+        component: PricingPage,
+      },
+      {
+        path: "/cars",
+        name: "cars",
+        component: CarsPage,
+      },
+      {
+        path: "/blog",
+        name: "blog",
+        component: BlogPage,
+      },
+      {
+        path: "/contact",
+        name: "contact",
+        component: ContactPage,
       },
     ],
   },
