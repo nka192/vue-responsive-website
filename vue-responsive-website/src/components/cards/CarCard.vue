a1255df07d5bb12db53585588d31aa549ccf4704
Nayoung Kim <nka50@sfu.ca>
2025-12-11 09:36:45 -0800

    style(car-card): update text sizes for name, brand, and price for better responsiveness

--- vue-responsive-website/src/components/cards/CarCard.vue
+++ vue-responsive-website/src/components/cards/CarCard.vue
@@ -2,10 +2,12 @@
   <div class="max-w-80 bg-white rounded-lg overflow-hidden shadow-md">
     <img :src="getImage(image)" :alt="name" class="w-full h-56 object-cover" />
     <div class="p-6 space-y-2">
-      <h3 class="text-xl font-medium text-gray-800">{{ name }}</h3>
+      <h3 class="text-sm md:text-xl font-medium text-gray-800">
+        {{ name }}
+      </h3>
       <div class="flex justify-between items-center">
-        <p class="text-md text-gray-400">{{ brand }}</p>
-        <p class="text-md text-blue-500 font-semibold">
+        <p class="text-sm md:text-md text-gray-400">{{ brand }}</p>
+        <p class="text-sm md:text-md text-blue-500 font-semibold">
           {{ price }}
           <span class="text-gray-400 text-sm font-light">/ day</span>
         </p>
