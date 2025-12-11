4c512bf1e817f695f7d6014eb6eda98441c4104d
Nayoung Kim <nka50@sfu.ca>
2025-12-11 11:16:53 -0800

    feat(home): add TestimonySection to home page with Swiper carousel

---
+++ vue-responsive-website/src/components/home/TestimonySection.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-gray-100/90">
+    <SectionHeading title="Happy Clients" description="Testimonial" />
+    <section class="py-10 md:py-20 p-4">
+      <Swiper
+        :modules="[Autoplay]"
+        :space-between="30"
+        loop
+        :autoplay="{ delay: 3000, disableOnInteraction: false }"
+        :breakpoints="{
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }"
+        class="w-full max-w-5xl"
+      >
+        <SwiperSlide v-for="(testimony, index) in testimonys" :key="index">
+          <TestimonyCard
+            :name="testimony.name"
+            :position="testimony.position"
+            :image="testimony.image"
+            :comment="testimony.comment"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import SectionHeading from "../SectionHeading.vue";
+import { testimonys } from "../../data/testimonyData";
+import TestimonyCard from "../cards/TestimonyCard.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+</script>
+
+<style></style>
--- vue-responsive-website/src/page/HomePage.vue
+++ vue-responsive-website/src/page/HomePage.vue
@@ -5,6 +5,7 @@
     <AboutUsSection />
     <ServiceListSection />
     <CallToActionSection />
+    <TestimonySection />
   </main>
 </template>
 
@@ -14,6 +15,7 @@ import FeaturedSection from "../components/home/FeaturedSection.vue";
 import AboutUsSection from "../components/home/AboutUsSection.vue";
 import ServiceListSection from "../components/home/ServiceListSection.vue";
 import CallToActionSection from "../components/home/CallToActionSection.vue";
+import TestimonySection from "../components/home/TestimonySection.vue";
 </script>
 
 <style></style>
