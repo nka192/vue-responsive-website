017a9e07b3487ab362d32ced8ea23f63276d4ce8
Nayoung Kim <nka50@sfu.ca>
2025-12-10 23:10:39 -0800

    feat(home): add CarCards component to FeaturedSection with buttons and pricing

---
+++ vue-responsive-website/src/components/cards/CarCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="max-w-80 bg-white rounded-lg overflow-hidden shadow-md">
+    <img :src="getImage(image)" :alt="name" class="w-full h-56 object-cover" />
+    <div class="p-6 space-y-2">
+      <h3 class="text-xl font-medium text-gray-800">{{ name }}</h3>
+      <div class="flex justify-between items-center">
+        <p class="text-md text-gray-400">{{ brand }}</p>
+        <p class="text-md text-blue-500 font-semibold">
+          {{ price }}
+          <span class="text-gray-400 text-sm font-light">/ day</span>
+        </p>
+      </div>
+      <div class="flex justify-between">
+        <ButtonComponent text="Book Now"></ButtonComponent>
+        <ButtonComponent
+          text="Details"
+          bgColor="bg-green-500"
+        ></ButtonComponent>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ButtonComponent from "../ButtonComponent.vue";
+
+defineProps({
+  id: Number,
+  name: String,
+  brand: String,
+  price: String,
+  image: String,
+});
+
+const getImage = (imageName) => {
+  return new URL(`/src/assets/images/${imageName}`, import.meta.url).href;
+};
+</script>
+
+<style></style>
--- vue-responsive-website/src/components/home/FeaturedSection.vue
+++ vue-responsive-website/src/components/home/FeaturedSection.vue
@@ -1,9 +1,42 @@
 <template>
-  <SectionHeading title="Featured Vehicles" description="What we offer" />
+  <main>
+    <SectionHeading title="Featured Vehicles" description="What we offer" />
+    <div class="w-full h-full py-10 md:py-20">
+      <Swiper
+        :modules="[Autoplay]"
+        :space-between="30"
+        navigation
+        loop
+        :autoplay="{ delay: 3000, disableOnInteraction: false }"
+        :breakpoints="{
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }"
+        class="w-4/5 drop-shadow-2xl"
+      >
+        <SwiperSlide v-for="(car, index) in cars" :key="index">
+          <CarCard
+            :id="car.id"
+            :name="car.name"
+            :brand="car.brand"
+            :price="car.price"
+            :image="car.image"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </main>
 </template>
 
 <script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+import CarCard from "../cards/CarCard.vue";
 import SectionHeading from "../SectionHeading.vue";
+import { cars } from "../../data/carsData";
 </script>
 
 <style></style>
