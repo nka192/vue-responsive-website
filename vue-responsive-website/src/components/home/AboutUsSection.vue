b2ac3f65ec14d3ec404a2bb6cef568b7eedc724e
Nayoung Kim <nka50@sfu.ca>
2025-12-11 10:11:06 -0800

    feat(home): add AboutUsSection to home page

---
+++ vue-responsive-website/src/components/home/AboutUsSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="relative w-full md:h-screen">
+    <div
+      class="sm:absolute bg-[#10b981] w-full sm:w-2/3 md:h-[92%] top-0 right-0 pb-10"
+    >
+      <div class="sm:absolute -inset-x-1/3 inset-y-20 mb-4 md:mb-0">
+        <img
+          src="../../assets/images/about.jpg"
+          alt="About Us Section Image"
+          class="md:max-w-lg h-auto object-cover"
+        />
+      </div>
+      <div
+        class="text-white px-4 sm:absolute right-1/5 sm:w-2/4 sm:transform sm:top-3/5 sm:-translate-y-2/3"
+      >
+        <h2 class="text-sm font-semibold mb-4 uppercase tracking-wide">
+          About Us
+        </h2>
+        <h1 class="text-2xl md:text-4xl font-semibold mb-4">
+          Welcome to Carbook
+        </h1>
+        <p class="text-base mb-6">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          voluptatem possimus expedita. Inventore reiciendis voluptatem dolor
+          neque expedita. Numquam minima molestiae maiores quo assumenda,
+          officiis veritatis? Praesentium sint molestias accusamus?
+        </p>
+        <p class="text-base">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          voluptatem possimus expedita. Inventore reiciendis voluptatem dolor
+          neque expedita. Numquam minima molestiae maiores quo assumenda,
+          officiis veritatis? Praesentium sint molestias accusamus?
+        </p>
+        <ButtonComponent text="Search Vehicle" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ButtonComponent from "../ButtonComponent.vue";
+</script>
+
+<style></style>
--- vue-responsive-website/src/page/HomePage.vue
+++ vue-responsive-website/src/page/HomePage.vue
@@ -2,12 +2,14 @@
   <main>
     <HeroSection />
     <FeaturedSection />
+    <AboutUsSection />
   </main>
 </template>
 
 <script setup>
 import HeroSection from "../components/home/HeroSection.vue";
 import FeaturedSection from "../components/home/FeaturedSection.vue";
+import AboutUsSection from "../components/home/AboutUsSection.vue";
 </script>
 
 <style></style>
