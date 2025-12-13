0bc2cfd559f03418efe75b7dd6f33f9096ce828a
Nayoung Kim <nka50@sfu.ca>
2025-12-13 03:45:55 -0800

    feat(services): add ServicesPage

---
+++ vue-responsive-website/src/page/ServicesPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <BgContainer
+      title="Services"
+      bgImage="bg_3.jpg"
+      :links="[
+        { text: 'Home', to: '/' },
+        { text: 'Services', to: '/services' },
+      ]"
+    />
+    <ServicesListSection />
+    <CallToActionSection />
+  </main>
+</template>
+
+<script setup>
+import BgContainer from "../components/BgContainer.vue";
+import ServicesListSection from "../components/ServiceListSection.vue";
+import CallToActionSection from "../components/CallToActionSection.vue";
+</script>
+
+<style></style>
