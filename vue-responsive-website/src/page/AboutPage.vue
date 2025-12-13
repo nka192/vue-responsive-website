0f3aaf73e43e04db28ceae7dbbe51ac5b2b42703
Nayoung Kim <nka50@sfu.ca>
2025-12-13 03:32:46 -0800

    feat(about): add AboutPage with sections

---
+++ vue-responsive-website/src/page/AboutPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <BgContainer
+      title="About"
+      bgImage="bg_3.jpg"
+      :links="[
+        { text: 'Home', to: '/' },
+        { text: 'About Us', to: '/about' },
+      ]"
+    />
+    <AboutUsSection />
+    <TestimonySection />
+    <StatisticsSection />
+  </main>
+</template>
+
+<script setup>
+import BgContainer from "../components/BgContainer.vue";
+import AboutUsSection from "../components/home/AboutUsSection.vue";
+import TestimonySection from "../components/home/TestimonySection.vue";
+import StatisticsSection from "../components/home/StatisticsSection.vue";
+</script>
+
+<style></style>
