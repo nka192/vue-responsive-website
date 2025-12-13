8114efd3cdaa3976bee6a1b0248e185b979591df
Nayoung Kim <nka50@sfu.ca>
2025-12-13 04:08:13 -0800

    feat(cars): add CarsPage

---
+++ vue-responsive-website/src/page/CarsPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <BgContainer
+      title="Cars"
+      bgImage="bg_3.jpg"
+      :links="[
+        { text: 'Home', to: '/' },
+        { text: 'Cars', to: '/cars' },
+      ]"
+    />
+    <div class="flex justify-center w-full my-20">
+      <div class="grid grid-cols-1 md:grid-cols-3 w-4/5 gap-10">
+        <CarCard
+          v-for="(car, index) in cars"
+          :key="index"
+          :id="car.id"
+          :name="car.name"
+          :price="car.price"
+          :image="car.image"
+          :brand="car.brand"
+        ></CarCard>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import BgContainer from "../components/BgContainer.vue";
+import CarCard from "../components/cards/CarCard.vue";
+import { cars } from "../data/carsData";
+</script>
+
+<style></style>
