796b2d60349b4834f2045e38d078dac7959a8a4a
Nayoung Kim <nka50@sfu.ca>
2025-12-12 23:57:13 -0800

    feat(home): add PageFooter with FooterLinks component

---
+++ vue-responsive-website/src/components/FooterLinks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-gray-200">
+    <h2 class="text-white text-xl font-semibold mb-2">{{ title }}</h2>
+    <ul class="text-gray-200 space-y-3 mt-6">
+      <li v-for="(link, index) in links" :key="index" class="cursor-pointer">
+        {{ link }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  links: Array,
+});
+</script>
+
+<style></style>
--- vue-responsive-website/src/components/layout/PageFooter.vue
+++ vue-responsive-website/src/components/layout/PageFooter.vue
@@ -1,9 +1,88 @@
 <template>
-  <h1>Footer</h1>
+  <div class="bg-black text-white p-4 md:p-20 py-10">
+    <div>
+      <div class="flex flex-col md:flex-row gap-10 justify-between">
+        <div class="md:w-1/5 space-y-6">
+          <h1 class="uppercase text-xl font-bold cursor-pointer">
+            <span class="text-white">Car</span>
+            <span class="text-green-500">Book</span>
+          </h1>
+          <p class="text-gray-200 text-base">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
+            architecto nobis itaque dolore et fuga pariatur.
+          </p>
+          <div class="flex items-center justify-between text-2xl w-1/2">
+            <BsTwitterX />
+            <CgFacebook />
+            <BsInstagram />
+          </div>
+        </div>
+        <FooterLinks :title="'Information'" :links="informationLinks" />
+        <FooterLinks
+          :title="'Customer Support'"
+          :links="customerSupportLinks"
+        />
+        <div class="md:w-1/5">
+          <h2 class="text-xl font-semibold mb-2">Have Questions?</h2>
+          <div class="flex flex-col space-y-4 mt-6">
+            <div
+              v-for="(info, index) in contactInfo"
+              :key="index"
+              class="flex items-start space-x-4"
+            >
+              <div>
+                <component :is="info.icon" class="text-md text-white" />
+              </div>
+              <p class="text-gray-200 text-md">{{ info.text }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p class="text-gray-200 text-md text-center mt-16">
+          Copyright ©2025 All rights reserved
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import {
+  BsTwitterX,
+  CgFacebook,
+  BsInstagram,
+  AkLocation,
+  AnOutlinedMail,
+  BsTelephoneFill,
+} from "@kalimahapps/vue-icons";
+import FooterLinks from "../FooterLinks.vue";
+
+const informationLinks = [
+  "About",
+  "Services",
+  "Terms and Conditions",
+  "Best Price",
+  "Guarantee",
+  "Privacy & Cookies Policy",
+];
+
+const customerSupportLinks = [
+  "FAQ",
+  "Payment Option",
+  "Booking Tips",
+  "How it Works",
+  "Contact Us",
+];
+
+const contactInfo = [
+  {
+    icon: AkLocation,
+    text: "203 Fake St. Mountain View, San Francisco, California, USA",
+  },
+  { icon: AnOutlinedMail, text: "+2 392 3929 210" },
+  { icon: BsTelephoneFill, text: "info@yourdomain.com" },
+];
 </script>
 
 <style></style>
